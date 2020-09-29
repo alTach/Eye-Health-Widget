c9bb2238087e7c3b1508d0dc264964a57e052ed4
taliev <taliev@luxoft.com>
2020-09-29 08:30:37 +0300

    - add moon around face

--- src/pages/home/scenes/Face.svelte
+++ src/pages/home/scenes/Face.svelte
@@ -8,16 +8,19 @@
         <Eye />
         <Eye />
     </div>
+    <div class="face__moon" style="transform: rotate(360deg)"></div>
 </div>
 
 
 <style lang="scss">
     @import "../../../styles/variable";
+    $face-w: 150px;
     .face {
-        width: 150px;
-        height: 150px;
+        width: $face-w;
+        height: $face-w;
         background-color: var(--primary);
         border-radius: 50%;
+        position: relative;
         &__eyes {
             display: flex;
             justify-content: space-between;
@@ -26,5 +29,22 @@
             position: relative;
             top: 34px;
         }
+        &__moon {
+            width: 10px;
+            height: 10px;
+            background: red;
+            border-radius: 50%;
+            position: absolute;
+            top: 10px;
+            left: 10px;
+            transform-origin: 65px 65px;
+            /*animation: moving .4s infinite linear*/
+
+        }
     }
+    @keyframes moving {
+        100% {transform: rotate(-360deg);}
+    }
+
+
 </style>
