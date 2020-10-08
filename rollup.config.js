06e879c4bf1b3d67a854f5e0a495076c94ea69f4
taliev <taliev@luxoft.com>
2020-10-08 23:44:45 +0300

    - update dependency;

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser'
-import json from 'rollup-plugin-json';
+import json from '@rollup/plugin-json';
 
 // SCSS
 import sveltePreprocess from 'svelte-preprocess';
